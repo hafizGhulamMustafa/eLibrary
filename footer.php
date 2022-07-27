851fffed9d75c8dcc661d6de40b0b1ec10530849
Ghulam-Mustafa <themustafadeveloper@gmail.com>
2022-07-27 15:15:44 +0500

    fyp-Elibrary

---
+++ footer.php
@@ -0,0 +1,16 @@
+<!-- FOOTER -->
+  <div id="footer">
+    <div class="container">
+        <div class="row ">
+            <div class="col-md-12">
+                <span>© Copyright 2022 <a href="#">Ghulam Mustafa & Ehtasham & Muqadas</a></span>
+            </div>
+        </div>
+    </div>
+  </div>
+  <!-- /FOOTER -->
+  <script src="js/jquery-3.6.0.min.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>E-Library Website</title>
+
+    <link rel="stylesheet" href="css/bootstrap.css">
+
+    <link rel="stylesheet" href="css/style.css">
+
+        <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+
+
+<link href="//fonts.googleapis.com/css?family=Playfair+Display:400,700%7COpen+Sans:300,400,600,700" rel="stylesheet">
+
+
+<link rel="stylesheet" type="text/css" href="assets/vendors/bootstrap/css/bootstrap.css">
+
+
+<link rel="stylesheet" type="text/css" href="assets/vendors/font-awesome/css/fontawesome-all.min.css">
+
+
+<link rel="stylesheet" type="text/css" href="assets/css/styles.css">
+
+  </head>
+  <body>
+
+ <section class="js-parallax u-promo-block u-promo-block--mheight-700 u-overlay u-overlay--dark text-white" style="background-image: url(assets/img/mainImage.jpg);">
+
+        <div class="container u-overlay__inner u-ver-center u-content-space">
+          <div class="row justify-content-center">
+            <div class="col-12">
+              <div class="text-center">
+                <p class="text-uppercase u-letter-spacing-sm mb-0">A Digital Library for EveryOne</p>
+                <h1 class="display-sm-4 display-lg-3 mb-3"><span class="js-display-typing"></span></h1>
+                <a class="btn btn-primary" href="index1.php">
+                  Access Library <i class="fas fa-arrow-alt-circle-right ml-2"></i>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+       
+      </section>
+     
+
+      <script src="assets/vendors/jquery.min.js"></script>
+    <script src="assets/vendors/jquery.migrate.min.js"></script>
+    <script src="assets/vendors/bootstrap/js/bootstrap.min.js"></script>
+
+    <script src="assets/vendors/typedjs/typed.min.js"></script>
+
+      <script>
+      $(document).on('ready', function () {
+        $(".js-display-typing").typed({
+          strings: [
+            "Welcome to My Digital Library",
+            "Just Like Pocket Library",
+            "This is me Ghulam Mustafa "
+          ],
+          typeSpeed: 60,
+          loop: true,
+          backDelay: 2500
+        });
+      });
+    </script>
+  </body>
+</html>
